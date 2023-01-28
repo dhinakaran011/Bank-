df0c8c1d9cb11f0dcac5a3fb0aba2cf7b80e3e67
dhinakaran s <53988061+dhinakaran011@users.noreply.github.com>
2023-01-28 17:06:08 +0530

    SBI.java

---
+++ SBI.java
@@ -0,0 +1,23 @@
+package bank.chennai;
+
+public class SBI{
+
+public String empName;
+
+public String empId;
+
+public static String branch_name = "chennai";
+
+public static void main(String args[]){
+
+}
+
+public void get_loan(int amount){
+
+}
+
+public void create_account(){
+
+}
+
+}
